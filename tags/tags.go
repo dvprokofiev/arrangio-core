f4cba60edb6e90dcf013a3eaba3fb9616ed38a9d
dvprokofiev <d@dvprokofiev.ru>
2026-05-25 21:02:12 +0300

    feat(tags): implement tagging system
    - tagging logic into a standalone 'tags' package
    - support 64 fast tags and unlimited dynamic tags via sorted slices

---
+++ tags/tags.go
@@ -0,0 +1,70 @@
+package tags
+
+import "sort"
+
+type Mask struct {
+	// tags which are used more often, that others
+	FastBits uint64
+
+	// use this if there are more than 64 tags
+	DynamicIDs []uint16
+}
+
+func NewMask() Mask {
+	return Mask{
+		FastBits:   0,
+		DynamicIDs: make([]uint16, 0, 4),
+	}
+}
+
+// check if mask contains tags or group of tags
+func (m Mask) Has(other Mask) bool {
+	if (m.FastBits & other.FastBits) != other.FastBits {
+		return false
+	}
+
+	if len(other.DynamicIDs) == 0 {
+		return true
+	}
+
+	// use binary search
+	for _, tagID := range other.DynamicIDs {
+		// find the idx of the element in m.DynamicIDs which is
+		// greater or equals some tagID in other.DynamicIDs
+		idx := sort.Search(len(m.DynamicIDs), func(i int) bool {
+			return m.DynamicIDs[i] >= tagID
+		})
+		// if the element not found or there's something else
+		// which is not equal to tagID
+		if idx == len(m.DynamicIDs) || m.DynamicIDs[idx] != tagID {
+			return false
+		}
+	}
+
+	return true
+}
+
+// add tag to the Mask
+func (m Mask) With(tagID int) Mask {
+	if tagID < 64 {
+		m.FastBits |= (1 << uint64(tagID))
+		return m
+	}
+
+	dynID := uint16(tagID)
+
+	// search index in which tagID will be inserted
+	idx := sort.Search(len(m.DynamicIDs), func(i int) bool {
+		return m.DynamicIDs[i] >= dynID
+	})
+
+	// if all elements in m.DynamicIDs are smaller than tagID
+	// or tagID must be inserted in between existent elements
+	if idx == len(m.DynamicIDs) || m.DynamicIDs[idx] != dynID {
+		m.DynamicIDs = append(m.DynamicIDs, 0)
+		copy(m.DynamicIDs[idx+1:], m.DynamicIDs[idx:])
+		m.DynamicIDs[idx] = dynID
+	}
+
+	return m
+}
